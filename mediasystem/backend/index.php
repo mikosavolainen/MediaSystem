005d5e9fbaa4bf337437a51e8f205e95314f6b52
mikosavolainen <miko.markus2112@gmail.com>
2024-11-13 13:13:30 +0200

    hehe

--- mediasystem/backend/index.php
+++ mediasystem/backend/index.php
@@ -1,17 +1,44 @@
-<?PHP 
-$serverName="37.136.11.1";
-$userName="root";
-$password="1234592";
-$databaseName="mediaserver";
-$conn = mysqli_connect($serverName, $userName, $password, $databaseName);
+<?php
+// MySQL Connection
+$serverName = "37.136.11.1";
+$userName = "root";
+$password = "1234592";
+$databaseName = "mediaserver";
 
+// MongoDB Connection
+require '../backend/c'; // Make sure MongoDB library is installed with Composer
+$mongoClient = new MongoDB\Client("mongodb://root:1234592@37.136.11.1:27017");
+$mongoDatabase = $mongoClient->mediaserver;
+$mongoCollection = $mongoDatabase->react_php;
+
+// Get the text from POST request
 $recText = $_POST['text'];
 
-$query = ("INSERT INTO react_php (texts) VALUES('$recText')");
-
-if (mysqli_query($conn, $query)) {
-  echo "Data has been inserted successfully";
-}else {
-  echo "Error";
+// MySQL query
+$conn = mysqli_connect($serverName, $userName, $password, $databaseName);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+// MySQL query to insert data
+$mysqlQuery = "INSERT INTO react_php (texts) VALUES ('$recText')";
+if (mysqli_query($conn, $mysqlQuery)) {
+    echo "Data has been inserted into MySQL successfully.<br>";
+} else {
+    echo "Error inserting into MySQL: " . mysqli_error($conn) . "<br>";
+}
+
+// MongoDB query to insert data
+$mongoDocument = ['texts' => $recText];
+$mongoResult = $mongoCollection->insertOne($mongoDocument);
+
+// Check MongoDB insertion result
+if ($mongoResult->getInsertedCount() > 0) {
+    echo "Data has been inserted into MongoDB successfully.";
+} else {
+    echo "Error inserting into MongoDB.";
+}
+
+// Close MySQL connection
+mysqli_close($conn);
 ?>
